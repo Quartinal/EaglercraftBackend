f9be2ec4909fcb0a5aa88a21c0be298f1f41bf95
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-02-04 15:38:31 -0500

    Updated application.rb and render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-npm i -g bun
-
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module EaglercraftBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.precompile += %w( application.css ).grep_v(/\.sass$/)
   end
 end
